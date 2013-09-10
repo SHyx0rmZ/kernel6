140c5d29b752f5c7c60c5f658fada4bfbf27418c
Patrick Pokatilo <mail@shyxormz.net>
2013-09-10 22:31:33 +0200

    + loader: Handle interrupts and IRQs

--- src/loader/isr.cpp
+++ src/loader/isr.cpp
@@ -1,6 +1,31 @@
 #include "cpustate.hpp"
+#include "console.hpp"
+#include "io.hpp"
 
 extern "C" cpustate *isr_handler(cpustate *state)
 {
+    if (state->vector < 0x20)
+    {
+        Console cout;
+
+        cout << "Exception encountered, stopping execution!" << endl << endl;
+
+        cout << "EIP " << (void*)state->eip << endl;
+        cout << "VEC " << (void*)state->vector << endl;
+        cout << "ERR " << (void*)state->errorcode << endl;
+
+        while (true)
+            asm volatile("hlt");
+    }
+    else if (state->vector < 0x30)
+    {
+        if (state->vector >= 0x28)
+        {
+            io::outb(0xA0, 0x20);
+        }
+
+        io::outb(0x20, 0x20);
+    }
+
     return state;
 }
