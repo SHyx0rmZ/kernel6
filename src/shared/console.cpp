bf59af00fc38e51604ef6a1c3e408601864f2bc1
Patrick Pokatilo <mail@shyxormz.net>
2016-01-03 00:07:52 +0000

    + Console now also prints to serial port

--- src/shared/console.cpp
+++ src/shared/console.cpp
@@ -3,15 +3,43 @@
 #include <cstring>
 
 #include "console.hpp"
+#include "io.hpp"
+
+#define CONSOLE_USE_SERIAL 1
 
 #define CONSOLE_AREA 0xB8000
 #define CONSOLE_START_LINE 2
 #define CONSOLE_START CONSOLE_AREA + CONSOLE_START_LINE * 160
 #define CONSOLE_COLOR 0x0700
 
+#if CONSOLE_USE_SERIAL == 1
+#define SERIAL_DIVISOR_LATCH_REGISTER_LSB   0x3F8
+#define SERIAL_TRANSMITTER_HOLDING_REGISTER 0x3F8
+#define SERIAL_RECEIVER_BUFFER_REGISTER     0x3F8
+#define SERIAL_DIVISOR_LATCH_REGISTER_MSB   0x3F9
+#define SERIAL_INTERRUPT_ENABLE_REGISTER    0x3F9
+#define SERIAL_FIFO_CONTROL_REGISTER        0x3FA
+#define SERIAL_LINE_CONTROL_REGISTER        0x3FB
+#define SERIAL_MODEM_CONTROL_REGISTER       0x3FC
+#define SERIAL_LINE_STATUS_REGISTER         0x3FD
+
+#define SERIAL_WORD_LENGTH_SELECTOR_8BIT        0x03
+#define SERIAL_TRANSMITTER_HOLDING_EMPTY_BIT    0x20
+#define SERIAL_DIVISOR_LATCH_ACCESS_BIT         0x80
+#endif
 
 Console::Console()
 {
+#if CONSOLE_USE_SERIAL == 1
+    io::outb(SERIAL_LINE_CONTROL_REGISTER, SERIAL_DIVISOR_LATCH_ACCESS_BIT | SERIAL_WORD_LENGTH_SELECTOR_8BIT);
+    io::outb(SERIAL_DIVISOR_LATCH_REGISTER_LSB, 0x0C);
+    io::outb(SERIAL_DIVISOR_LATCH_REGISTER_MSB, 0x00);
+    io::outb(SERIAL_LINE_CONTROL_REGISTER, SERIAL_WORD_LENGTH_SELECTOR_8BIT);
+    io::outb(SERIAL_INTERRUPT_ENABLE_REGISTER, 0);
+    io::outb(SERIAL_FIFO_CONTROL_REGISTER, 0);
+    io::outb(SERIAL_MODEM_CONTROL_REGISTER, 0);
+#endif
+
     std::memset(reinterpret_cast<void *>(CONSOLE_AREA), 0, 25 * 160);
 
     this->video = reinterpret_cast<std::uint16_t *>(CONSOLE_AREA);
@@ -31,6 +59,12 @@ Console &Console::operator<<(const char *const str)
 
     while (*character)
     {
+#if CONSOLE_USE_SERIAL == 1
+        io::outb(SERIAL_TRANSMITTER_HOLDING_REGISTER, *character);
+
+        while ((io::inb(SERIAL_LINE_STATUS_REGISTER) & SERIAL_TRANSMITTER_HOLDING_EMPTY_BIT) != SERIAL_TRANSMITTER_HOLDING_EMPTY_BIT);
+#endif
+
         switch (*character)
         {
             case '\n':
